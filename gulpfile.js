43831520f86b714fc0a88fd68a71067e1db8e9de
Joanna Hughes <jhughes@vitamix.com>
2019-03-24 13:16:57 -0400

    adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+// Gulp.js configuration
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var data = require('gulp-data');
+var autoprefixer = require('gulp-autoprefixer');
+
+// ------------------------------------------
+// Sass
+// ------------------------------------------
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', '> 2%', 'safari >= 7'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// ------------------------------------------
+// BrowserSync - Page Reload
+// ------------------------------------------
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+// ------------------------------------------
+// Watch Tasks
+// ------------------------------------------
+gulp.task('watch', ['browserSync', 'sass'], function (){
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+});
